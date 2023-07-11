5f24d0f4dc614f5d4dcde472d0f7795ff466e5f7
Dmytro Truten <dmitriy2503a@gmail.com>
2023-07-11 14:33:37 +0300

    rewrite navbar with react-bootstrap components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,46 +1,46 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavbarToggle from "react-bootstrap/NavbarToggle";
+import Image from "react-bootstrap/Image";
 import githubIcon from "../assets/icons/github-icon.svg";
 import twitterIcon from "../assets/icons/twitter-icon.svg";
 import linkedInIcon from "../assets/icons/linkedin-icon.svg";
 import "../styles/Navbar/Navbar.css";
 
-function Navbar() {
+function NavBar() {
   return (
-    <nav className="navbar navbar-expand-md pt-3 pt-sm-5">
-      <div className="inner-navbar container-fluid px-3 px-sm-5 px-xl-0">
-        <p className="navbar-brand m-0">{`{Truten}`}</p>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="offcanvas"
-          data-bs-target="#offcanvasNavbar"
-          aria-controls="offcanvasNavbar"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse justify-content-end">
-          <ul className="navbar-nav justify-content-between">
-            <li className="nav-item">Home</li>
-            <li className="nav-item">About</li>
-            <li className="nav-item">Tech Stack</li>
-            <li className="nav-item">Projects</li>
-            <li className="nav-item">Contact</li>
-          </ul>
-          <div className="navbar-socials-container d-flex justify-content-between ms-5">
-            <a href="#">
-              <img className="navbar-socials-icon" src={githubIcon} alt="" />
-            </a>
-            <a href="#">
-              <img className="navbar-socials-icon" src={twitterIcon} alt="" />
-            </a>
-            <a href="#">
-              <img className="navbar-socials-icon" src={linkedInIcon} alt="" />
-            </a>
-          </div>
-        </div>
-      </div>
-    </nav>
+    <Navbar expand="lg" className="pt-lg-5">
+      <Container className="px-3 px-sm-0">
+        <Navbar.Brand className="mx-0">{`{Truten}`}</Navbar.Brand>
+        <NavbarToggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Nav className="justify-content-between">
+            <Nav.Item>Home</Nav.Item>
+            <Nav.Item>About</Nav.Item>
+            <Nav.Item>Tech Stack</Nav.Item>
+            <Nav.Item>Projects</Nav.Item>
+            <Nav.Item>Contact</Nav.Item>
+          </Nav>
+          <Container className="navbar-socials-container d-flex justify-content-between ms-5 me-0 px-0">
+            <Nav.Link href="#">
+              <Image className="navbar-socials-icon" src={githubIcon} alt="" />
+            </Nav.Link>
+            <Nav.Link href="#">
+              <Image className="navbar-socials-icon" src={twitterIcon} alt="" />
+            </Nav.Link>
+            <Nav.Link href="#">
+              <Image
+                className="navbar-socials-icon"
+                src={linkedInIcon}
+                alt=""
+              />
+            </Nav.Link>
+          </Container>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
-export default Navbar;
+export default NavBar;
