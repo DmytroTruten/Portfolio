dd8efe5c4dea3b89f0bda93007963074b01392a1
Dmytro Truten <dmitriy2503a@gmail.com>
2023-07-11 21:35:31 +0300

    remove first hero component structure

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,33 +7,7 @@ import "../styles/Hero/Hero.css";
 
 function Hero() {
   return (
-    <Container fluid>
-      <Row>
-        <Col>
-          <Row>
-            <Col xs={12}>
-              <h1 className="hero-h1">Hi,</h1>
-            </Col>
-            <Col xs={12}>
-              <h1 className="hero-h1">My name is</h1>
-            </Col>
-            <Col xs={12}>
-              <h1 className="hero-h1 hero-name">Dmytro Truten</h1>
-            </Col>
-            <Col xs={12}>
-              <h1 className="hero-h1">I build things for web</h1>
-            </Col>
-          </Row>
-        </Col>
-        <Col xs={{ span: 12, order: "first" }} className="px-3">
-          <Container className="hero-img-container px-0 d-flex justify-content-center">
-            <div className="hero-img-wrapper">
-              <Image className="hero-img" src={trutenImg} roundedCircle />
-            </div>
-          </Container>
-        </Col>
-      </Row>
-    </Container>
+    <></>
   );
 }
 
