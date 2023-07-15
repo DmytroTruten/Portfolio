45808079ffa7e68dc7dd68e4ba86c07b60fd6e71
Dmytro Truten <dmitriy2503a@gmail.com>
2023-07-15 19:09:30 +0300

    add offcanvas to the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavbarToggle from "react-bootstrap/NavbarToggle";
 import Image from "react-bootstrap/Image";
+import Offcanvas from "react-bootstrap/Offcanvas";
 import githubIcon from "../assets/icons/github-icon.svg";
 import twitterIcon from "../assets/icons/twitter-icon.svg";
 import linkedInIcon from "../assets/icons/linkedin-icon.svg";
@@ -11,33 +13,49 @@ import "../styles/Navbar/Navbar.css";
 function NavBar() {
   return (
     <Navbar expand="lg" className="pt-lg-4 pb-0">
-      <Container className="navbar-inner-container">
+      <Container>
         <Navbar.Brand className="mx-0">{`{Truten}`}</Navbar.Brand>
-        <NavbarToggle />
-        <Navbar.Collapse className="justify-content-end">
-          <Nav className="justify-content-between">
-            <Nav.Item>Home</Nav.Item>
-            <Nav.Item>About</Nav.Item>
-            <Nav.Item>Tech Stack</Nav.Item>
-            <Nav.Item>Projects</Nav.Item>
-            <Nav.Item>Contact</Nav.Item>
-          </Nav>
-          <Container className="navbar-socials-container d-flex justify-content-between ms-5 me-0 px-0">
-            <Nav.Link href="#">
-              <Image className="navbar-socials-icon" src={githubIcon} alt="" />
-            </Nav.Link>
-            <Nav.Link href="#">
-              <Image className="navbar-socials-icon" src={twitterIcon} alt="" />
-            </Nav.Link>
-            <Nav.Link href="#">
-              <Image
-                className="navbar-socials-icon"
-                src={linkedInIcon}
-                alt=""
-              />
-            </Nav.Link>
-          </Container>
-        </Navbar.Collapse>
+        <NavbarToggle aria-controls="offcanvasNavbar-expand-lg" />
+        <Navbar.Offcanvas
+          id="offcanvasNavbar-expand-lg"
+          aria-labelledby="offcanvasNavbarLabel-expand-lg"
+          placement="end"
+        >
+          <Offcanvas.Header closeButton className="justify-content-end" />
+
+          <Offcanvas.Body className="justify-content-end">
+            <Nav className="offcanvas-body-nav justify-content-between">
+              <Nav.Item className="my-2 my-lg-0">Home</Nav.Item>
+              <Nav.Item className="my-2 my-lg-0">About</Nav.Item>
+              <Nav.Item className="my-2 my-lg-0">Tech Stack</Nav.Item>
+              <Nav.Item className="my-2 my-lg-0">Projects</Nav.Item>
+              <Nav.Item className="my-2 my-lg-0">Contact</Nav.Item>
+            </Nav>
+            <Container className="navbar-socials-container d-flex justify-content-between my-3 my-lg-0 ms-lg-5 me-lg-0 px-0">
+              <Nav.Link href="#">
+                <Image
+                  className="navbar-socials-icon"
+                  src={githubIcon}
+                  alt=""
+                />
+              </Nav.Link>
+              <Nav.Link href="#">
+                <Image
+                  className="navbar-socials-icon"
+                  src={twitterIcon}
+                  alt=""
+                />
+              </Nav.Link>
+              <Nav.Link href="#">
+                <Image
+                  className="navbar-socials-icon"
+                  src={linkedInIcon}
+                  alt=""
+                />
+              </Nav.Link>
+            </Container>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
       </Container>
     </Navbar>
   );
